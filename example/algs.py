68fcf82189747841bf5270c5240cca195540a0f2
Elizabeth McCarthy <elizabeth@Elizabeths-MacBook-Pro-2.local>
2019-01-22 14:25:34 -0800

    Update files for hw 1

--- example/algs.py
+++ example/algs.py
@@ -1,25 +1,62 @@
 import numpy as np
-
-def pointless_sort(x):
-    """
-    This function always returns the same values to show how testing
-    works, check out the `test/test_alg.py` file to see.
-    """
-    return np.array([1,2,3])
-
 def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
-
-    assert 1 == 1
+  """
+  Describe how you are sorting `x`:
+  Start at the beginning of the list and for each pair of elements
+  i and i+1 flip the order if i+1 is smaller than i.
+  Iterate over the list pairwise until the list is sorted.
+  """
+  try: 
+    test = [i == int(i) for i in x]
+    if sum(test)!=len(x):
+      return "Please provide a list made ONLY of integers."
+  except ValueError:
+    return 'Please provide a list made ONLY of integers.'
+  if (len(x) > 1):
+    j=1
+    while j!=0:
+      j=0
+      for i in range(len(x)-1):    
+        if x[i] > x[i+1]:
+          x[i],x[i+1] = x[i+1],x[i]
+          j = j+1
     return x
+  else:
+    return x
+
+def partition(x):
+  lower_index=1
+  higher_index=len(x)-1
+  while higher_index > lower_index:
+    while (x[lower_index] <= x[0]) & ((lower_index+1)<len(x)):
+      lower_index = lower_index+1
+    while (x[higher_index] > x[0]) & ((higher_index-1) > -1):
+      higher_index = higher_index-1
+    if higher_index > lower_index:
+      x[higher_index],x[lower_index] = x[lower_index], x[higher_index]                                     
+  return higher_index
+      
 
 def quicksort(x):
     """
     Describe how you are sorting `x`
+    Choose a pivot. Arrange all the elements so the larger elements are in front of the pivot
+    and the smaller elements are behind the pivot. Then recursively apply the funciton on the sublists.
     """
-
-    assert 1 == 1
-    return
+    try: 
+      test = [i == int(i) for i in x]
+      if sum(test)!=len(x):
+        return "Please provide a list made ONLY of integers."
+    except ValueError:
+      return 'Please provide a list made ONLY of integers.'
+      
+    if (len(x) < 2):
+        return x
+    else:
+        if x[0] > x[partition(x)]:
+          pivot = partition(x)
+          x[0], x[pivot] = x[pivot], x[0]
+        smaller = quicksort(x[:partition(x)])
+        larger = quicksort(x[(partition(x)+1):])
+        return smaller + [x[partition(x)]] + larger
 
--- test/test_algs.py
+++ test/test_algs.py
@@ -1,34 +1,84 @@
 import numpy as np
 from example import algs
 
-def test_pointless_sort():
-    # generate random vector of length 10
-    x = np.random.rand(10)
-
-    # check that pointless_sort always returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
-
-    # generate a new random vector of length 10
-    x = np.random.rand(10)
-
-    # check that pointless_sort still returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
-
 def test_bubblesort():
-    # Actually test bubblesort here. It might be useful to think about
-    # some edge cases for your code, where it might fail. Some things to
-    # think about: (1) does your code handle 0-element arrays without
-    # failing, (2) does your code handle characters?
+  #Empty vector
+  x = []
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
 
-    x = np.array([1,2,4,0,1])
-    
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
-    algs.bubblesort(x)
+  #One entry
+  x = [1]
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
+
+  #Character entry
+  x = ["a",1,2]
+  #check
+  assert algs.bubblesort(x) == "Please provide a list made ONLY of integers."
+
+  #Non-integer entry
+  x = [0.5,1,2]
+  #check
+  assert algs.bubblesort(x) == "Please provide a list made ONLY of integers."
+
+  #Repeating entry (case 1)
+  x = [6,1,3,6]
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
+
+  #Repeating entry (case 2)
+  x = [8,9,8,9]
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
+
+  #Odd length entry
+  x = [7,4,5]
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
+
+  #Even length entry
+  x = [7,4,5,1]
+  #check
+  assert np.array_equal(algs.bubblesort(x), sorted(x))
 
 def test_quicksort():
+  #Empty vector
+  x = []
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
 
-    x = np.array([1,2,4,0,1])
-    # for now, just attempt to call the quicksort function, should
-    # actually check output
-    algs.quicksort(x)
+  #One entry
+  x = [1]
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
+
+  #Character entry
+  x = ["a",1,2]
+  #check
+  assert algs.quicksort(x) == "Please provide a list made ONLY of integers."
+
+  #Non-integer entry
+  x = [0.5,1,2]
+  #check
+  assert algs.quicksort(x) == "Please provide a list made ONLY of integers."
+
+  #Repeating entry (case 1)
+  x = [6,1,3,6]
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
+
+  #Repeating entry (case 2)
+  x = [8,9,8,9]
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
+
+  #Odd length entry
+  x = [7,4,5]
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
+
+  #Even length entry
+  x = [7,4,5,1]
+  #check
+  assert np.array_equal(algs.quicksort(x), sorted(x))
